227be58875ab597832ab149de301630f4a6fc6cd
sparkxxxxxx <5482855@qq.com>
2018-12-09 19:14:09 +0800

    init project

---
+++ src/renderer/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'pan'
+  }
+</script>
+
+<style>
+  /* CSS */
+</style>
