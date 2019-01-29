be0b0eb8022003aa0fe71bb55e4b87592a40b5bd
Iron <5482855@qq.com>
2019-01-29 13:31:01 +0800

    Revert "引入ts解析"

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -5,15 +5,14 @@
 </template>
 
 <script>
-    export default {
-        name: 'pan'
-    };
+  export default {
+    name: 'pan'
+  }
 </script>
 
 <style lang="scss">
   /* CSS */
   body {
-    user-select: none;
     height: 100vh;
     min-width: 960px;
     min-height: 650px;
@@ -25,8 +24,8 @@
   }
 
   .clearfix:after {
-    content: '';
-    display:block;
+    content: ''; 
+    display:block; 
     clear:both;
   }
 </style>
